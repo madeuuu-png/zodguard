53ecdb07327ea6f76fe8a0ee74c996ce162e0346
made19palomino <made19palomino@gmail.com>
2025-11-11 05:18:30 +0000

    ✨ feat(auth): añadir TextMessage de validación para registro

--- lib/schemans/TextMessage.ts
+++ lib/schemans/TextMessage.ts
@@ -1,10 +1,22 @@
 export const TextMessages = {
-    emailRequired: 'El email es obligatorio',
-    emailMissingAt: 'El email debe contener el símbolo "@"',
-    emailInvalidFormat: 'El formato del email es inválido (ej: usuario@dominio.com)',
-    passwordTooShort: 'La contraseña debe tener al menos 4 caracteres',
-    passwordTooLong: 'La contraseña no puede tener más de 12 caracteres',
-    passwordMissingUpper: 'La contraseña debe contener al menos una letra mayúscula',
-    passwordMissingNumber: 'La contraseña debe contener al menos un número',
-    passwordMissingSpecial: 'La contraseña debe contener al menos un carácter especial',
-  };
+  // Email
+  emailRequired: 'El email es obligatorio',
+  emailMissingAt: 'El email debe contener el símbolo "@"',
+  emailInvalidFormat: 'El formato del email es inválido (ej: usuario@dominio.com)',
+  
+  // Password
+  passwordTooShort: 'La contraseña debe tener al menos 4 caracteres',
+  passwordTooLong: 'La contraseña no puede tener más de 12 caracteres',
+  passwordMissingUpper: 'La contraseña debe contener al menos una letra mayúscula',
+  passwordMissingNumber: 'La contraseña debe contener al menos un número',
+  passwordMissingSpecial: 'La contraseña debe contener al menos un carácter especial',
+  
+  // Name
+  nameRequired: 'El nombre es obligatorio',
+  nameTooShort: 'El nombre debe tener al menos 3 caracteres',
+  nameTooLong: 'El nombre no puede tener más de 50 caracteres',
+  
+  // Confirm Password
+  confirmPasswordRequired: 'Debes confirmar tu contraseña',
+  passwordsDoNotMatch: 'Las contraseñas no coinciden',
+};
