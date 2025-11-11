d9c217968c6a25fd412570d31c5cd317ed70a9da
made19palomino <made19palomino@gmail.com>
2025-11-11 05:22:10 +0000

    ♻️ refactor(screens): separar login en pantalla independiente

---
+++ app/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import LoginForm from '../components/loginForm';
+
+export default function LoginScreen() {
+  return (
+    <SafeAreaView className="flex-1 bg-purple-50">
+      <LoginForm />
+    </SafeAreaView>
+  );
+}
